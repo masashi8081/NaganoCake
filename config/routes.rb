8f3c8626a27fca528271b2d5f404a48e7523bb5c
masashi8081 <126481445+masashi8081@users.noreply.github.com>
2023-05-03 06:29:32 +0000

    0503

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    get  "/" => "homes#top"
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :items, only: [:new, :show, :edit, :index, :create, :update]
+    resources :orders, only: [:new, :index, :show]
+  end
+
+  scope module:  :public do
+    resources :cart_items, only: [:index, :create, :destroy, :update]
+    resources :customers, only: [:show, :update, :edit, :quit]
+    resources :items, only: [:index, :show]
+    root to: "homes#top"
+    get '/home/about' => 'homes#about', as: 'about'
+  end
+
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: 'admin/sessions'
   }
@@ -9,7 +24,5 @@ Rails.application.routes.draw do
     sessions: 'public/sessions'
   }
 
-  root to: 'homes#top'
-  get '/home/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
